d630658431f1305d455a66ab9915cb51a976e5ab
TheNormanJames <normane_@hotmail.com>
2025-03-12 16:42:22 -0500

    Clase 365

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+// import router from "./router";
+import server from "./server";
+import colors from "colors";
 
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+  console.log(colors.cyan.bold(`Rest API en ${port}`));
+});
