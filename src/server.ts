f5da850780347a65525f49e39a12ddbe374780ad
NormanJaimes <normane_@hotmail.com>
2025-03-14 22:34:30 -0500

    test: :test_tube: Add: test for get/api

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ async function connnectDB() {
   try {
     await db.authenticate();
     db.sync();
-    console.log(colors.blue('Conexión exitosa a la Db'));
+    // console.log(colors.blue('Conexión exitosa a la Db'));
   } catch (error) {
     console.log(error);
     console.log(colors.red.bold('Error al conectar la base de datos'));
@@ -23,5 +23,9 @@ const server = express();
 server.use(express.json());
 
 server.use('/api/products', router);
+
+server.get('/api', (req, res) => {
+  res.json({ msg: 'Desde API' });
+});
 
 export default server;
