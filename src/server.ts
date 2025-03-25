54afb78cb2d62fa7539cfd226a80e8e20cf7c361
NormanJaimes <normane_@hotmail.com>
2025-03-24 20:26:18 -0500

    feat: :bug: fix some bug

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,6 @@ export async function connnectDB() {
   try {
     await db.authenticate();
     db.sync();
-    // console.log(colors.blue('Conexión exitosa a la Db'));
   } catch (error) {
     // console.log(error);
     console.log(colors.red.bold('Error al conectar la base de datos'));
